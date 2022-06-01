9e1ca116bea75bbe237f15a5a7d9e22371bcfefb
Great Nwamadi <nwamadigreat@gmail.com>
2022-06-01 12:58:32 +0100

    Set proxy to localhost after pulling from main

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            // target: 'http://localhost:4000',
-            target: 'https://ngotechprojectexplorer.herokuapp.com',
+            target: 'http://localhost:4000',
+            // target: 'https://ngotechprojectexplorer.herokuapp.com',
             changeOrigin: true,
         })
     );
