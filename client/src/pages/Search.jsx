c573f59bca3d0ec42b3431e11bd89674a01f4e19
Great Nwamadi <nwamadigreat@gmail.com>
2022-07-07 01:19:04 +0100

    finished searching

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -5,6 +5,7 @@ import { Row, Container, Col, Card, Form, FormControl, Button, InputGroup, Pagin
 import Layout from "./shared/Layout";
 import AuthService from '../services/auth';
 import Loader from "../components/Loader";
+import { useSearchParams } from "react-router-dom";
 
 const Search = () => {
   
@@ -13,18 +14,31 @@ const Search = () => {
   const [numOfResult, setNumOfResult] = useState(0);
   const [numOfPage, setNumOfPage] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
-  let [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
   const [search, setSearch] = useState('');
-  const [searchBy, setSearchBy] = useState('');
+  const [searchBy, setSearchBy] = useState(''||"name");
+
+  // Use Search params
+  const [searchParams, setSearchParams] = useSearchParams();
+
+
+  console.log("The search params", searchParams.get("search") )
 
   useEffect(() => {
+    let searchFromNav = searchParams.get("search");
+    let searchByFromNav = searchParams.get("searchBy");
 
-    fetch('/api/search')
+    console.log("Searchhhhh ", searchFromNav);
+    let url;
+    searchFromNav == null ? url = `/api/search` : url = `/api/search?search=${searchFromNav}`;
+    fetch(url)
       .then(async (response) => {
         const res = await response.json();
         console.log("The result ", res);
         setNumOfResult(res.allProjects)
         setCurrentPage(res.currentPage);
+        setPage(res.currentPage);
+        // console.log("The current page ", currentPage)
         setNumOfPage(res.numOfPages);
         const { data } = res
         console.log('The projects ', data);
@@ -32,7 +46,15 @@ const Search = () => {
         setIsLoading(false);
       })
 
-  }, [])
+  }, [searchParams])
+  
+  console.log("The current page ", currentPage)
+  console.log("The page ", page)
+  console.log(page===1)
+  console.log("The pages number of result ", numOfPage)
+
+  console.log("The first page equals 1", page + " T/f ", page===1);
+
   
 // On submit search result
 const handleSearch = (evt) => {
@@ -47,9 +69,12 @@ const handleSearch = (evt) => {
     console.log("The result ", res);
     setNumOfResult(res.allProjects)
     setCurrentPage(res.currentPage);
+    setPage(res.currentPage);
     setNumOfPage(res.numOfPages);
     const { data } = res
     console.log('The projects ', data);
+    setSearchParams({search, searchBy})
+
     setProjects(data);
     setIsLoading(false);
   })
@@ -57,38 +82,19 @@ const handleSearch = (evt) => {
   
 }
 
-const result = () => {
-  setIsLoading(true)    //Load Loader component
-
-  fetch(`/api/search?search=${search}&searchBy=${searchBy}&page=${currentPage}`)
-  .then(async (response) => {
-    const res = await response.json();
-    console.log("The result ", res);
-    setNumOfResult(res.allProjects)
-    setCurrentPage(res.currentPage);
-    setNumOfPage(res.numOfPages);
-    const { data } = res
-    console.log('The projects ', data);
-    setProjects(data);
-    setIsLoading(false);
-  })
- 
-}
-
-  //On Click first button
   const handleClickFirst = (evt) => {
     evt.preventDefault();
  
-    
+    setPage(1)  //Load the first page
     setIsLoading(true)    //Load Loader component
 
     fetch(`/api/search?search=${search}&searchBy=${searchBy}`)
     .then(async (response) => {
       const res = await response.json();
       console.log("The result ", res);
-      setNumOfResult(res.allProjects)
       setCurrentPage(res.currentPage);
-      setNumOfPage(res.numOfPages);
+      setNumOfResult(res.allProjects)
+     
       const { data } = res
       console.log('The projects ', data);
       setProjects(data);
@@ -106,13 +112,13 @@ const result = () => {
     
     setIsLoading(true)    //Load Loader component
 
-    fetch(`/api/search?search=${search}&searchBy=${searchBy}&page=${page}`)
+    fetch(`/api/search?search=${search}&searchBy=${searchBy}&page=${page-1}`)
     .then(async (response) => {
       const res = await response.json();
       console.log("The result ", res);
       setNumOfResult(res.allProjects)
       setCurrentPage(res.currentPage);
-      setNumOfPage(res.numOfPages);
+      // setNumOfPage(res.numOfPages);
       const { data } = res
       console.log('The projects ', data);
       setProjects(data);
@@ -125,18 +131,16 @@ const result = () => {
   const handleClickNext = (evt) => {
     evt.preventDefault();
 
-    console.log("The page ", page)
     setPage(page+1);
-    
     setIsLoading(true)    //Load Loader component
 
-    fetch(`/api/search?search=${search}&searchBy=${searchBy}&page=${page}`)
+    fetch(`/api/search?search=${search}&searchBy=${searchBy}&page=${page+1}`)
     .then(async (response) => {
       const res = await response.json();
       console.log("The result ", res);
       setNumOfResult(res.allProjects)
       setCurrentPage(res.currentPage);
-      setNumOfPage(res.numOfPages);
+      // setNumOfPage(res.numOfPages);
       const { data } = res
       console.log('The projects ', data);
       setProjects(data);
@@ -150,6 +154,7 @@ const result = () => {
     evt.preventDefault();
     
     setIsLoading(true)    //Load Loader component
+    setPage(numOfPage);
 
     fetch(`/api/search?search=${search}&searchBy=${searchBy}&page=${numOfPage}`)
     .then(async (response) => {
@@ -157,13 +162,26 @@ const result = () => {
       console.log("The result ", res);
       setNumOfResult(res.allProjects)
       setCurrentPage(res.currentPage);
-      setNumOfPage(res.numOfPages);
+      // setNumOfPage(res.numOfPages);
       const { data } = res
       console.log('The projects ', data);
       setProjects(data);
       setIsLoading(false);
     })
   
+  }
+
+  const handleChange = (evt) => {
+    const {name, value} = evt.target;
+
+    if(name === "search") {
+      setSearch(value);
+
+    } else if(name === "searchBy") {
+      setSearchBy(value);
+
+    }
+    
   }
 
   console.log("Search ", search)
@@ -189,7 +207,7 @@ const result = () => {
                           placeholder="Search Project name, authors, abstract, tags"
                           className="me-2 mt-1"
                           aria-label="Search"
-                          onChange={(evt) => setSearch(evt.target.value)}
+                          onChange={handleChange}
                           // required
                         />
                     </Col>
@@ -199,14 +217,15 @@ const result = () => {
                         <Form.Select
                           name="searchBy"
                           value={searchBy}
-                          onChange={(evt) => setSearchBy(evt.target.value) }
+                          onChange={handleChange }
                           required
                         >
+                           
                             <option value="name">Name</option>
                             <option value="abstract">Abstract</option>
                             <option value="authors">Authors</option>
                             <option value="tags">Tags</option>
-                            <option value="">All</option>
+                            {/* <option value="">All</option> */}
                        
                           </Form.Select>
 
@@ -220,9 +239,6 @@ const result = () => {
                 
                   </Row>
                 </Form>
-
-
-
            
             </nav>
 
@@ -235,54 +251,54 @@ const result = () => {
           {isLoading ? <Loader size={"150px"}/> : 
 
           <>
-            {/* Data from api is always 8 results if it's up to 8 ... since im using css grid and breakpoints there's no need to slice the results */}
-            <Row xs={1} lg={4} md={2} sm={2} className="g-4 showcase">
-              {projects&&projects.map((project) => (
+            {numOfResult < 1 ? 
+              <Row xs={1} lg={4} md={2} sm={2} className="g-4 showcase">
+                  <h3>No results found</h3>
+              </Row> 
 
-                <Col key={project._id} className="projCard" >
-                  <Card keys={project._id} className="singleCard">
-                    <Card.Body keys={project._id}>
-                        <Card.Title > <a href={`/projects/${project._id}`} keys={project.name} style={{textDecoration:"none", color:"#198754", fontWeight:"bold"}}>{project.name} </a>  </Card.Title>
-                        <Card.Link href="#" className="projectAuthors">
-                          {project.authors.join(",")}
-                        </Card.Link>
-                        <Card.Text className="projectText">{project.abstract.substring(0, 100)} ...</Card.Text>
-                        <Card.Link href="#" className="projectTags"> {project.tags.join('#')}</Card.Link>
-                        {/* <p>Last visited</p> */}
-                     </Card.Body>
-                  </Card>
-                </Col>
-                ))}
-            </Row>
+              : 
+              <>
+                {/* Data from api is always 8 results if it's up to 8 ... since im using css grid and breakpoints there's no need to slice the results */}
+                <Row xs={1} lg={4} md={2} sm={2} className="g-4 showcase">
+                        {projects&&projects.map((project) => (
+
+                          <Col key={project._id} className="projCard" >
+                            <Card keys={project._id} className="singleCard">
+                              <Card.Body keys={project._id}>
+                                  <Card.Title > <a href={`/projects/${project._id}`} keys={project.name} style={{textDecoration:"none", color:"#198754", fontWeight:"bold"}}>{project.name} </a>  </Card.Title>
+                                  <Card.Link href="#" className="projectAuthors">
+                                    {project.authors.join(",")}
+                                  </Card.Link>
+                                  <Card.Text className="projectText">{project.abstract.substring(0, 100)} ...</Card.Text>
+                                  <Card.Link href="#" className="projectTags"> {project.tags.join('#')}</Card.Link>
+                                  {/* <p>Last visited</p> */}
+                              </Card.Body>
+                            </Card>
+                          </Col>
+                          ))}
+                      </Row>
+                        </>
+          
+            }
+        
 
             </>
             
           }
 
             <Container>
+
               <Row>
                 <Col>
                   <Pagination className="justify-content-center">
-                    <Pagination.First onClick={handleClickFirst} disabled={currentPage===1 ? true : false}>First</Pagination.First>
-                    <Pagination.Prev onClick={handleClickPrevious} disabled={currentPage===1 ? true : false}>Previous</Pagination.Prev>
+                    <Pagination.First onClick={handleClickFirst} disabled={page<=1 ? true : false}></Pagination.First>
+                    <Pagination.Prev onClick={handleClickPrevious} disabled={page<=1 ? true : false}></Pagination.Prev>
                     <Pagination.Item active>{currentPage}</Pagination.Item>
-                    <Pagination.Next onClick={handleClickNext} disabled={currentPage===numOfPage ? true : false} >Next</Pagination.Next>
-                    <Pagination.Last onClick={handleClickLast} disabled={currentPage===numOfPage ? true : false}>Last</Pagination.Last>
+                    <Pagination.Next onClick={handleClickNext} disabled={page===numOfPage || page === 0 ? true : false} ></Pagination.Next>
+                    <Pagination.Last onClick={handleClickLast} disabled={page===numOfPage || page === 0 ? true : false}></Pagination.Last>
                   </Pagination>          
                 </Col>
               </Row>
-         
-              {/* <Row>
-                <Col>
-                  <Pagination className="justify-content-center">
-                    <Pagination.First />
-                    <Pagination.Prev />
-                    <Pagination.Item active>{1}</Pagination.Item>
-                    <Pagination.Next />
-                    <Pagination.Last />
-                  </Pagination>          
-                </Col>
-              </Row> */}
 
             </Container>
           </Container>
